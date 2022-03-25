0ea552f3f82fcaa469b73d8817866715e9bddddb
Andrés Ramos Palacios <andreschennai@gmail.com>
2022-03-25 08:28:16 -0600

    c

--- src/views/MfeTwo-RouteTwo.vue
+++ src/views/MfeTwo-RouteTwo.vue
@@ -1,7 +1,15 @@
 <template>
-  <span>Hello from route two</span>
+  <img class="center" src="../assets/diagram.png" />
 </template>
 
 <script setup>
 
 </script>
+
+<style>
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}</style>
